cf41aa976003498c27716e60963bcdfce1cd40b6
Dmitrii933 <dborshik@yandex.ru>
2022-12-07 19:56:14 +0300

    update

--- src/test/java/CucumberRunner_Test.java
+++ src/test/java/CucumberRunner_Test.java
@@ -1,18 +1,5 @@
+import io.cucumber.junit.platform.engine.Cucumber;
 
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/com",
-        dryRun = false,
-        tags = "@all",
-        snippets = CucumberOptions.SnippetType.UNDERSCORE,
-        glue = {"com.steps","com.hooks"}
-)
-
+@Cucumber
 public class CucumberRunner_Test {
 }
